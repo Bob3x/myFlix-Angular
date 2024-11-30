84690a7d2f7147fac206e373ae3834f948c259d3
Bob3x <bob3xx@protonmail.com>
2024-12-01 00:27:31 +0200

    Adding FormsModule

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
 
 import { routes } from "./app.routes";
 import { provideClientHydration, withEventReplay } from "@angular/platform-browser";
@@ -10,6 +12,8 @@ export const appConfig: ApplicationConfig = {
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideClientHydration(withEventReplay()),
-        provideHttpClient()
+        provideHttpClient(),
+        provideAnimations(),
+        FormsModule
     ]
 };
