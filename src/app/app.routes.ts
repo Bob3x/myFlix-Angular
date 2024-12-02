de8801584a05e15d2c2971bb52bed9037461f22d
Bob3x <bob3xx@protonmail.com>
2024-12-02 03:53:35 +0200

    Creating routes between views using RouterModule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { MovieCardComponent } from "./movie-card/movie-card.component";
+import { WelcomePageComponent } from "./welcome-page/welcome-page.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "welcome", component: WelcomePageComponent },
+    { path: "movies", component: MovieCardComponent },
+    { path: "profile", component: UserProfileComponent },
+    { path: "", redirectTo: "welcome", pathMatch: "prefix" },
+    { path: "**", redirectTo: "welcome" }
+];
